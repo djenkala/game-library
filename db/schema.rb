75f66b81ecdfaaa01ff68a893aa3dcb6bfc1397d
Daniel Jenkala <daniel@jenkala.com>
2020-03-29 12:43:39 -0500

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200329011941) do
+ActiveRecord::Schema.define(version: 20200329174144) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
+    t.string "password_digest"
   end
 
 end
