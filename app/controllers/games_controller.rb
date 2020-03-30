9f4c7764bd40743895b5c0a7224d6af143cafaea
Daniel Jenkala <daniel@jenkala.com>
2020-03-29 19:16:57 -0500

    Update games_controller.rb

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,9 +15,10 @@ class GamesController < ApplicationController
   get '/games/:id/edit' do
     if !logged_in?
       redirect "/login"
+    elsif game = current_user.games.find_by(params[:id])
+      "Edit a game form. #{current_user.id} is editing game #{game.id}"
     else
-      game = current_user.games.find_by(params[:id])
-      "Edit a game form"
+      redirect '/games'
     end
   end
 
