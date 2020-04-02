d2f9ebe0625dd917c7f14fd74ff3fd606221d33c
Daniel Jenkala <daniel@jenkala.com>
2020-04-01 22:17:09 -0500

    Update games_controller.rb

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,32 +1,23 @@
 class GamesController < ApplicationController
 
   get '/games' do
-    if !logged_in?
-      redirect "/login"
-    else
+    redirect_if_not_logged_in
       @games = current_user.games
       erb :"games/index.html"
     end
-  end
 
   get '/games/new' do
-    if !logged_in?
-      redirect "/login"
-    else
+    redirect_if_not_logged_in
       erb :"games/new_game.html"
     end
-  end
 
   get '/games/:id/edit' do
-    if !logged_in?
-      redirect "/login"
-    else
+    redirect_if_not_logged_in
       @game = Game.find(params[:id])
       if @game.user_id == current_user.id
       erb :"games/edit.html"
     else
       redirect '/games'
-    end
     end
   end
 
@@ -38,9 +29,7 @@ class GamesController < ApplicationController
   end
 
   get '/games/:id' do
-    if !logged_in?
-      redirect "/login"
-    end
+    redirect_if_not_logged_in
     @game = Game.find_by(id: params[:id])
     erb :"games/show.html"
   end
