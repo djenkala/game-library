bea72dc7f1beb8a79ae647aff1613558d0105488
Daniel Jenkala <daniel@jenkala.com>
2020-04-06 23:00:37 -0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,14 @@ class UsersController < ApplicationController
     erb :"users/signup.html"
   end
 
+  #cannot create games with no name
+  #cant signup with existing user email (belongs to a current user)
+
   post '/users' do
     @user = User.new
     @user.email = params[:email]
     @user.password = params[:password]
-    if @user.save
+    if !@user.email.blank? && !@user.password.blank? && @user.save
       session[:email] = @user.email
       redirect '/games'
     else
